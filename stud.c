b8c3ab63d4165bfdc2e95699a1162c733ba45c1a
joywin2003 <21a20.joywin@sjec.ac.in>
2023-03-24 19:51:50 +0530

    edited stud.c

--- stud.c
+++ stud.c
@@ -27,6 +27,56 @@ char *filename = "students.bin";
 int hash(const char *name) {
     return isalpha(name[0]) ? toupper(name[0]) - 'A' : 0;
 }
+student *createStudentNode(char *name) {
+    student *node = malloc(sizeof(student));
+    if (!node) return NULL;
+    strncpy(node->name, name, MAX_NAME_LEN);
+    printf("Enter student USN: ");
+    fgets(node->usn, MAX_USN_LEN, stdin);
+    printf("Enter student Phone: ");
+    fgets(node->phone, MAX_PHONE_LEN, stdin);
+    marksEntry(node);
+    attendanceEntry(node);
+    node->next = NULL;
+    return node;
+}
+
+int insertStudent(char *name) {
+    int position = hash(name);
+    student *node = createStudentNode(name);
+    if (!node){
+        printf("Failed to insert the student details!!\n");
+        return 0;
+    }
+    if (!table[position]) 
+        table[position] = node;
+    else {
+        student *cursor = table[position];
+        while (cursor->next) {
+            cursor = cursor->next;
+        }
+        cursor->next = node;
+    }
+    printf("Student details inserted successfully!!\n");
+    return 1;
+}
+
+void getStudent(char *name) {
+    int position = hash(name);
+    student *cursor = table[position];
+    int count = 0;
+    while (cursor) {
+        if (strncmp(cursor->name, name, MAX_NAME_LEN) == 0) {
+            printf("Student found with name '%s' \n", name);
+            DisplayStudent(cursor);
+            count++;
+        }
+        cursor = cursor->next;
+    }
+    if (count == 0) {
+        printf("No student found with name '%s'\n", name);
+    }
+}
 
 void marksEntry(student *name){
     int total=0;
@@ -50,70 +100,6 @@ void attendanceEntry(student *name){
         name->attendance[i] = (x/y)*100.0;
     }
 }
-
-void marksDisplay(student *name){
-    printf("*******MARKS CARD*********\n");
-    for(int i=1;i<10;i++){
-        printf("||SUBJECT CODE:%s||MAKRS:%lf||\n",subject_code[i],name->marks[i]);
-    }
-    printf("||TOTAL PERCENTAGE=%lf%%",name->avg);
-}
-
-void attendanceDisplay(student *name){ 
-    printf("*******ATTENDANCE CARD********\n");
-    for(int i=1;i<10;i++){
-        printf("||SUBJECT CODE:%s||ATTENDANCE(X/Y):%lf%%\n",subject_code[i],name->attendance[i]);
-    }
-}
-student *createStudentNode(char *name) {
-    student *node = malloc(sizeof(student));
-    if (!node) return NULL;
-    strncpy(node->name, name, MAX_NAME_LEN);
-    printf("Enter student USN: ");
-    fgets(node->usn, MAX_USN_LEN, stdin);
-    printf("Enter student Phone: ");
-    fgets(node->phone, MAX_PHONE_LEN, stdin);
-    marksEntry(node);
-    attendanceEntry(node);
-    node->next = NULL;
-    return node;
-}
-
-int insertStudent(char *name) {
-    int position = hash(name);
-    student *node = makeNode(table[position], name);
-    if (!node){
-        printf("Failed to insert the student details\n");
-        return 0;
-    }
-    if (!table[position]) 
-        table[position] = node;
-    else {
-        student *cursor = table[position];
-        while (cursor->next) {
-            cursor = cursor->next;
-        }
-        cursor->next = node;
-    }
-    return 1;
-}
-
-void getStudent(char *name) {
-    int position = hash(name);
-    student *cursor = table[position];
-    int count = 0;
-    while (cursor) {
-        if (strncmp(cursor->name, name, MAX_NAME_LEN) == 0) {
-            printf("Student found with name '%s' \n", name);
-            DisplayStudent(cursor);
-            count++;
-        }
-        cursor = cursor->next;
-    }
-    if (count == 0) {
-        printf("No student found with name '%s'\n", name);
-    }
-}
 void DisplayStudent(student *name){
     int position = hash(name);
     student *cursor = table[position];
@@ -124,40 +110,20 @@ void DisplayStudent(student *name){
     printf("==  STUDENT USN :   %-15s  ==\n",cursor->usn);
     printf("==  STUDENT PHNO:   %-15s  ==\n",cursor->phone);
     printf("======================================\n");
+    printf("=====================================\n");
+    printf("==       ATTENDANCE DETAILS        ==\n");
+    printf("======================================\n");
+    for(int i=1;i<10;i++){
+        printf("SUBJECT CODE: %s||ATTENDANCE: %lf%%\n",subject_code[i],name->attendance[i]);
+    }
+    printf("======================================\n");
+    printf("==         MARKS DETAILS            ==\n");
+    printf("======================================\n");
+    for(int i=1;i<10;i++){
+        printf("||SUBJECT CODE:%s||MAKRS:%lf||\n",subject_code[i],name->marks[i]);
+    }
+    printf("==   TOTAL PERCENTAGE=%lf%%         ==\n",name->avg);
     
-}
-void saveStudentsToFile() {
-    FILE *file = fopen(filename, "wb");
-    if (!file) {
-        printf("Failed to open file: %s\n", filename);
-        return;
-    }
-    for (int i = 0; i < 26; i++) {
-        student *cursor = table[i];
-        while (cursor) {
-            fwrite(cursor, sizeof(student), 1, file);
-            cursor = cursor->next;
-        }
-    }
-    fclose(file);
-}
-void loadStudentsFromFile() {
-    FILE *file = fopen(filename, "rb");
-    if (!file) {
-        printf("Failed to open file: %s\n", filename);
-        return;
-    }
-    student tmp;
-    while (fread(&tmp, sizeof(student), 1, file) == 1) {
-        insertStudent(tmp.name);
-        int position = hash(tmp.name);
-        student *cursor = table[position];
-        while (cursor->next) {
-            cursor = cursor->next;
-        }
-        *cursor = tmp;
-    }
-    fclose(file);
 }
 
 void main(){
@@ -166,11 +132,10 @@ void main(){
     printf("==== ENTER YOUR CHOICE ====\n");
     printf("1. Insert student details\n");
     printf("2. Find student details\n");
-    printf("3. Display student marks card\n");
-    printf("4. Display student attendance\n");
-    printf("5. Save student details to file\n");
-    printf("6. Load student details from file\n");
-    printf("7. Exit\n");
+    printf("3. Enter the student marks\n");
+    printf("4. Enter the student attendance\n");
+    printf("5. Display student performance card\n");
+    printf("6. Exit\n");
     scanf("%d",&choice);
     switch (choice){
     case 1:
@@ -186,10 +151,21 @@ void main(){
         break;
     case 3:
         printf("Enter student name: ");
-        
+        marksEntry(name);
         break;
-
+    case 4: 
+        printf("Enter student name: ");
+        attendanceEntry(name);
+        break;
+    case 5: 
+        printf("Enter student name: ");
+        DisplayStudent(name);
+        break;
+    case 6:
+        printf("Exiting...\n");
+        break;
     default:
+        printf("Invalid choice\n");
         break;
     }
 }
